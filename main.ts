2267024f9e7f39b23b07b946587972b8b56b57e2
Robert Thomitzek <thro@thomitzek.com>
2020-05-12 17:43:26 +0200

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,19 @@
-
+//% weight=10 color=#008B00 icon="\uf136"
+//% blockId=i2c_motor_shield 
+namespace i2c_motor_shield {
+export function MotorRun (chanel: number, speed: number): void {
+    let buf = pins.createBuffer(3);
+if (speed >= 0) {
+        buf[0] = 2;
+    } else {
+        buf[0] = 3;
+    }
+    buf[1] = chanel;
+    if (speed >= 0) {
+        buf[2] = speed;
+    } else {
+        buf[2] = speed * -1;
+    }
+    pins.i2cWriteBuffer(0x14, buf);
+}
+}
