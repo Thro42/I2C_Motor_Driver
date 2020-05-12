05ef746b60d40527cce0aa56b978d2ae45b1a65d
Robert Thomitzek <thro@thomitzek.com>
2020-05-12 20:43:29 +0200

    blockID

--- main.ts
+++ main.ts
@@ -1,19 +1,19 @@
 //% weight=10 color=#008B00 icon="\uf136"
-//% blockId=i2c_motor_shield 
 namespace i2c_motor_shield {
-export function I2CmotorRun (chanel: number, speed: number): void {
-    let buf = pins.createBuffer(3);
-if (speed >= 0) {
-        buf[0] = 2;
-    } else {
-        buf[0] = 3;
+    //% block
+    export function I2CmotorRun (chanel: number, speed: number): void {
+        let buf = pins.createBuffer(3);
+        if (speed >= 0) {
+                buf[0] = 2;
+        } else {
+                buf[0] = 3;
+        }
+        buf[1] = chanel;
+        if (speed >= 0) {
+                buf[2] = speed;
+        } else {
+                buf[2] = speed * -1;
+        }
+        pins.i2cWriteBuffer(0x14, buf);
     }
-    buf[1] = chanel;
-    if (speed >= 0) {
-        buf[2] = speed;
-    } else {
-        buf[2] = speed * -1;
-    }
-    pins.i2cWriteBuffer(0x14, buf);
-}
 }
